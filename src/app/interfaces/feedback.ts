a8423381330cb442faf10a37e84f581f101becec
PC CASA <luishfmendes226@gmail.com>
2025-11-15 19:58:14 -0300

    Estruturação e Style Footer 1.0

---
+++ src/app/interfaces/feedback.ts
@@ -0,0 +1,10 @@
+export interface Feedback {
+    categoriaIcone: string;
+    categoriaNome: string;
+    comentario: string;
+    avatar: string;
+    nome: string;
+    data: string;
+
+    rating: number;
+}
