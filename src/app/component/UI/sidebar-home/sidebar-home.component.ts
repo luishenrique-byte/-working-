0c095190b74a4ceaa2ec7d41e5376231a8f075f5
Luís <luishfmendes226@gmail.com>
2025-11-17 16:47:07 -0300

    feat(sidebar): Criei o inicio da sidebar do home

---
+++ src/app/component/UI/sidebar-home/sidebar-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-home',
+  imports: [],
+  templateUrl: './sidebar-home.component.html',
+  styleUrl: './sidebar-home.component.css'
+})
+export class SidebarHomeComponent {
+
+}
