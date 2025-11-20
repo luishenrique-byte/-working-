61f41cbdf16ce3cec80f498c0cf3429aa59dacde
PC CASA <luishfmendes226@gmail.com>
2025-11-20 16:07:17 -0300

    style(call-to-action): Adicionando animação de surgimento na img

--- src/app/pages/home/section-call-to-action/section-call-to-action.component.ts
+++ src/app/pages/home/section-call-to-action/section-call-to-action.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-section-call-to-action',
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './section-call-to-action.component.css'
 })
 export class SectionCallToActionComponent {
+  
+  @ViewChild('imagemAnimada') imagemRef!: ElementRef;
 
+  ngAfterViewInit() {
+    // 2. Criamos o observador (o vigia)
+    const observador = new IntersectionObserver((entradas) => {
+      entradas.forEach(entrada => {
+        // Se a imagem apareceu na tela...
+        if (entrada.isIntersecting) {
+
+          // Adiciona a classe do CSS que faz ela descer e aparecer
+          entrada.target.classList.add('aparecer');
+
+          // E manda o vigia parar de olhar (assim ela não some mais)
+          observador.unobserve(entrada.target);
+        }
+      });
+    }, { threshold: 0.3 }); // 0.3 significa: aciona quando 30% da imagem estiver visível
+
+    // 3. Começa a vigiar a imagem
+    if (this.imagemRef) {
+      observador.observe(this.imagemRef.nativeElement);
+    }
+  }
 }
