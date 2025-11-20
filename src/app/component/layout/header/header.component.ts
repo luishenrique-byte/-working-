1b9b7514615685de7f80ed19395c5dbc5c65f9da
PC CASA <luishfmendes226@gmail.com>
2025-11-20 13:18:08 -0300

    fix(sidebar): Correção do Espaço Vazia e do Toque Fantasma

--- src/app/component/layout/header/header.component.ts
+++ src/app/component/layout/header/header.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { SidebarHomeComponent } from '../../UI/sidebar-home/sidebar-home.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SidebarHomeComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isMenuOpen: Boolean = false;
+  private timer: any;
+
+  joinMouse():void{
+    clearTimeout(this.timer);
+
+    this.isMenuOpen =true;
+  }
+
+  exitMouse(): void{
+    this.timer = setTimeout(() => {
+      this.isMenuOpen=false;
+    } , 500)
+  }
 
 }
