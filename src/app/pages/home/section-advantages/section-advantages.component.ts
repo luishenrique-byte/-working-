226aa2dd0b25cef2a708d8cee2a52736a7614426
PC CASA <luishfmendes226@gmail.com>
2025-11-20 21:36:00 -0300

    feat(section-vantagem): Implementação da section de Vantagem

--- src/app/pages/home/section-advantages/section-advantages.component.ts
+++ src/app/pages/home/section-advantages/section-advantages.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-section-advantages',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './section-advantages.component.html',
   styleUrl: './section-advantages.component.css'
 })
 export class SectionAdvantagesComponent {
 
+  public advantages = [
+    {
+      icon: 'assets/img/icons/Clock.svg',
+      title: 'Gestão de Tempo',
+      text1: 'Pare de perder tempo pulando entre Workana, Fiverr e Upwork. Nosso dashboard centraliza todos os seus projetos, prazos e comunicações em um único lugar.',
+      text2: 'Recupere o controle do seu dia.'
+    },
+    {
+      icon: 'assets/img/icons/Icon group.png',
+      title: 'Gestão de Tempo',
+      text1: 'O "vício de fazer tudo sozinho" limita seu crescimento. Convide seus parceiros de confiança ou publique seu projeto em nossa comunidade para encontrar novos talentos.',
+      text2: 'Escalar nunca foi tão fácil.'
+    },
+    {
+      icon: 'assets/img/icons/Icon Kanban.svg',
+      title: 'Gestão de Tempo',
+      text1: 'Uma equipe precisa de organização. Use nosso quadro Kanban integrado para visualizar tarefas, definir prazos e acompanhar o progresso do projeto em tempo real.',
+      text2: 'Gestão de verdade, sem sair do site.'
+    }
+  ]
+
+  @ViewChild('sectionRef') sectionRef!: ElementRef;
+
+  ngAfterViewInit() {
+    const observador = new IntersectionObserver((entradas) => {
+      entradas.forEach(entrada => {
+        if (entrada.isIntersecting) {
+          
+          // Adiciona a classe .aparecer NA SECTION INTEIRA
+          entrada.target.classList.add('aparecer');
+
+          // Opcional: Se quiser animar os cards também, pode buscar eles aqui
+          // e adicionar a classe neles, mas as barras já vão funcionar.
+          
+          observador.unobserve(entrada.target);
+        }
+      });
+    }, { threshold: 0.3 }); // Dispara quando 30% da section amarela aparecer
+
+    if (this.sectionRef) {
+      observador.observe(this.sectionRef.nativeElement);
+    }
+  }
 }
