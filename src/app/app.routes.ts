f9deeb8aef54ee8b83bfaea67722accd54b59355
Luís <luishfmendes226@gmail.com>
2025-11-26 16:51:26 -0300

    fix(app.routes): ajuste de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WorkspaceComponent } from './pages/workspace/workspace.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'workspace', component: WorkspaceComponent},
+    {path: '**', redirectTo: ''}
+
+];
