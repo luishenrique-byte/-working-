14936df55a9464f99039d572ed43e25171479a88
PC CASA <luishfmendes226@gmail.com>
2025-11-26 21:36:59 -0300

    feat(board):  implementar funcionalidade de Grab to Scroll

--- src/app/pages/workspace/component/board/board.component.ts
+++ src/app/pages/workspace/component/board/board.component.ts
@@ -10,6 +10,52 @@ import { CommonModule } from '@angular/common';
   styleUrl: './board.component.css'
 })
 export class BoardComponent {
+
+
+  //==================================
+  //LÓGICA ATRÁS DO SCROLL DAS COLUNAS
+  //==================================
+  // VARIÁVEIS DE CONTROLE
+  isDown = false;       // O mouse está clicado? (A bandeira)
+  startX = 0;           // Onde o mouse estava quando clicou?
+  scrollLeft = 0;       // Onde a barra de rolagem estava quando clicou?
+
+  // 1. QUANDO CLICA (Segura o papel)
+  iniciarArrasto(e: MouseEvent, slider: HTMLDivElement) {
+    this.isDown = true;
+    slider.classList.add('active'); // Opcional: para mudar estilo no CSS
+    
+    // Matemática: Posição do Mouse na página - Margem da div
+    this.startX = e.pageX - slider.offsetLeft;
+    
+    // Guarda a posição atual do scroll
+    this.scrollLeft = slider.scrollLeft;
+  }
+
+  // 2. QUANDO SOLTA OU SAI DA TELA (Solta o papel)
+  pararArrasto() {
+    this.isDown = false;
+    // slider.classList.remove('active');
+  }
+
+  // 3. ENQUANTO MOVE O MOUSE (Arrasta o papel)
+  moverArrasto(e: MouseEvent, slider: HTMLDivElement) {
+    // Se o mouse não estiver clicado, não faz nada
+    if (!this.isDown) return; 
+    
+    e.preventDefault(); // Evita selecionar texto sem querer
+    
+    // Onde o mouse está AGORA?
+    const x = e.pageX - slider.offsetLeft;
+    
+    // Quanto o mouse andou? (Agora - Antes)
+    const andou = (x - this.startX); 
+    
+    // Move a barra de rolagem
+    // Multipliquei por 2 para ficar mais rápido (velocidade)
+    slider.scrollLeft = this.scrollLeft - andou;
+  }
+
   public Colunas: Coluna[] = [
     {
       nome: "A Fazer 📌",
@@ -23,7 +69,50 @@ export class BoardComponent {
       tarefas: [
         {id:3, titulo: 'Desenvolver página de pagamento', descricao:'NÃO ESQUECER A CHAVE PIX'}
       ]
-    },
+    }
+    ,
+    {
+      nome: "Concluído ✅",
+      tarefas: [
+        {id:4, titulo: 'Instalar angular', descricao:'comando para baixar dependencias: npm install'}
+      ]
+    }
+    ,
+    {
+      nome: "Concluído ✅",
+      tarefas: [
+        {id:4, titulo: 'Instalar angular', descricao:'comando para baixar dependencias: npm install'}
+      ]
+    }
+    ,
+    {
+      nome: "Concluído ✅",
+      tarefas: [
+        {id:4, titulo: 'Instalar angular', descricao:'comando para baixar dependencias: npm install'}
+      ]
+    }
+    ,
+    {
+      nome: "Concluído ✅",
+      tarefas: [
+        {id:4, titulo: 'Instalar angular', descricao:'comando para baixar dependencias: npm install'}
+      ]
+    }
+    ,
+    {
+      nome: "Concluído ✅",
+      tarefas: [
+        {id:4, titulo: 'Instalar angular', descricao:'comando para baixar dependencias: npm install'}
+      ]
+    }
+    ,
+    {
+      nome: "Concluído ✅",
+      tarefas: [
+        {id:4, titulo: 'Instalar angular', descricao:'comando para baixar dependencias: npm install'}
+      ]
+    }
+    ,
     {
       nome: "Concluído ✅",
       tarefas: [
