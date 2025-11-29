648a83659f98a30924b06be979f3a585b2d2ee66
PC CASA <luishfmendes226@gmail.com>
2025-11-29 13:40:52 -0300

    feat(pages login;signUp): acresentar paginas de login e signUp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { WorkspaceComponent } from './pages/workspace/workspace.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'workspace', component: WorkspaceComponent},
+    {path: 'auth/login', component: LoginComponent},
+    {path: 'auth/signUp', component: SignUpComponent},
     {path: '**', redirectTo: ''}
 
 ];
