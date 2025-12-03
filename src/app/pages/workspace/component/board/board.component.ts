ce05fd06b7cb57f80955f10b182825bd720d397a
Luís <luishfmendes226@gmail.com>
2025-12-03 16:33:46 -0300

    feat(task-details): criar popIp de detalhes task

--- src/app/pages/workspace/component/board/board.component.ts
+++ src/app/pages/workspace/component/board/board.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarBoardComponent } from "./sidebar-board/sidebar-board.component";
 import { QuadroComponent } from "./quadro/quadro.component";
+import { TaskDetailsComponent } from "./task-details/task-details.component";
 
 
 @Component({
   selector: 'app-board',
-  imports: [SidebarBoardComponent, QuadroComponent],
+  imports: [SidebarBoardComponent, QuadroComponent, TaskDetailsComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
